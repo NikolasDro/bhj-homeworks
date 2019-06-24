3c56bde31034a21cb1a6327773d2371bfd6350b9
Nikolas Drozhzhin <drozhzhinnm@gmail.com>
2019-06-24 20:13:25 +0500

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,28 +1,33 @@
 'use strict'
 
-let sliderPrev = document.getElementsByClassName('slider__arrow_prev').item(0);
-let sliderNext = document.getElementsByClassName('slider__arrow_next').item(0);
-let navigation = document.querySelector('.slider__arrows');
-let sliderItems = document.getElementsByClassName('slider__item');
-let sliderItemsArr = Array.from(sliderItems);
+//let sliderPrev = document.getElementsByClassName('slider__arrow_prev').item(0);
+//let sliderNext = document.getElementsByClassName('slider__arrow_next').item(0);
+let navigation = Array.from(document.getElementsByClassName('slider__arrow'));
+let sliderItems = Array.from(document.getElementsByClassName('slider__item'));
+//let sliderItemsArr = Array.from(sliderItems);
 let activeSlide = 0;
 
 
 
-navigation.onclick = function(event) {
-	if (event.target === sliderNext && activeSlide === sliderItemsArr.length - 1) {
-		sliderItems[sliderItemsArr.length - 1].classList.toggle('slider__item_active');
-		activeSlide = 0;
-	} else if (event.target === sliderNext) {
-		activeSlide++;
-		sliderItems[activeSlide - 1].classList.toggle('slider__item_active');
-	} else if (event.target === sliderPrev && activeSlide === 0) {
-		sliderItems[0].classList.toggle('slider__item_active');
-		activeSlide = sliderItemsArr.length - 1;
-	} else if (event.target === sliderPrev) {
-		activeSlide--;
-		sliderItems[activeSlide + 1].classList.toggle('slider__item_active');
+for (let item of navigation) {
+	if (item.className.includes('slider__arrow_next')) {
+		item.onclick = function() {
+			sliderItems[activeSlide].className = 'slider__item';
+			activeSlide++;
+			if (activeSlide === sliderItems.length) {
+			activeSlide = 0;
+			}
+			sliderItems[activeSlide].className = 'slider__item slider__item_active';
+		}
 	}
-
-	sliderItems[activeSlide].classList.toggle('slider__item_active');
+	if (item.className.includes('slider__arrow_prev')) {
+		item.onclick = function() {
+			sliderItems[activeSlide].className = 'slider__item';
+			activeSlide--;
+			if (activeSlide < 0 ) {
+				activeSlide = sliderItems.length - 1;
+			}
+			sliderItems[activeSlide].className = 'slider__item slider__item_active';
+		}
+	}
 }
